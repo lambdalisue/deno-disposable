8ae3752e3cf65c1b62a8c0d4c4e60a6d4039291e
Alisue <lambdalisue@hashnote.net>
2021-06-21 02:54:16 +0900

    Add `usingAll` and `usingAllSync`

--- using.ts
+++ using.ts
@@ -18,6 +18,23 @@ export async function using<T extends Disposable, R = unknown>(
 }
 
 /**
+ * Ensure synchronous and/or asynchronous disposable resources are disposed
+ *
+ * It invokes 'dispose()' method of resources prior to leave the inner function.
+ * It suppors both synchronous and asynchronous disposables.
+ */
+export async function usingAll<T extends readonly Disposable[], R = unknown>(
+  resources: [...T],
+  fn: (...resources: [...T]) => R | Promise<R>,
+): Promise<R> {
+  try {
+    return await fn(...resources);
+  } finally {
+    await Promise.all(resources.map((r) => r.dispose()));
+  }
+}
+
+/**
  * Ensure a synchronous disposable resource is disposed
  *
  * It invokes 'dispose()' method of the resource prior to leave the inner function.
@@ -31,5 +48,22 @@ export function usingSync<T extends Disposable<void>, R = unknown>(
     return fn(resource);
   } finally {
     resource.dispose();
+  }
+}
+
+/**
+ * Ensure synchronous disposable resources are disposed
+ *
+ * It invokes 'dispose()' method of resources prior to leave the inner function.
+ * It suppors only synchronous disposables.
+ */
+export function usingAllSync<T extends Disposable<void>[], R = unknown>(
+  resources: [...T],
+  fn: (...resources: [...T]) => R,
+): R {
+  try {
+    return fn(...resources);
+  } finally {
+    resources.forEach((r) => r.dispose());
   }
 }
